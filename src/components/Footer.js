585eb953773adf5c5756fabb24b1e88b3350ebac
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-04-01 18:56:51 -0300

    add tailwind css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Footer = () => {
     return (
-        <>
-            
-        </>
+        <footer>
+            <p className="font-bold text-center text-red-400">Derechos Reservadinhos 2024, Albion Tools by Pelotuda</p>
+        </footer>
     );
 };
