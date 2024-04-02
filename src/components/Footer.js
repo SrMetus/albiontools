9187d48d1e0bdfb03542496375dbeeb08f8c636e
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-04-01 23:21:18 -0300

    Add style in Navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Footer = () => {
     return (
-        <footer>
-            <p className="font-bold text-center text-red-400">Derechos Reservadinhos 2024, Albion Tools by Pelotuda</p>
+        <footer className="fixed bottom-0 w-full bg-gray-800 text-white text-center py-4">
+            <p className="font-bold">Derechos Reservadinhos 2024, Albion Tools by Pelotuda</p>
         </footer>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 //import "../styles/navbar.css";
 //import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <nav>
-        <h1>Albion Tools</h1>
-        <ul>
-            <li><a href="" className="text-red-400">Mercado</a></li>
-            <li><a href="">Calculadoras</a></li>
-        </ul>
-    </nav>
-  );
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    }
+    return (
+        <nav className="bg-gray-800 font-semibold">
+            <div className="container mx-auto px-4 py-6">
+                <div className="flex justify-center">
+                    <div className="flex space-x-4">
+                        <a href="#" className="text-white hover:bg-gray-600 rounded-md p-1">Inicio</a>
+                        <a href="#" className="text-white hover:bg-gray-600 rounded-md p-1">Mercado</a>
+                        <div className="relative" onClick={toggleDropdown}>
+                            <button className="text-white hover:bg-gray-600 rounded-md p-1">Calculadora</button>
+                            <div className={`absolute bg-gray-700 mt-1 p-3 rounded-md ${dropdownOpen ? '' : 'hidden'}`}>
+                                <a href="#" className="block text-white hover:bg-gray-600 rounded-md p-1">Pesca</a>
+                                <a href="#" className="block text-white hover:bg-gray-600 rounded-md p-1">Peleteria</a>
+                                <a href="#" className="block text-white hover:bg-gray-600 rounded-md p-1">Mineria</a>
+                                <a href="#" className="block text-white hover:bg-gray-600 rounded-md p-1">Cocina</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
