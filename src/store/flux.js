59e14f7ef815dd83f3c4a4e74b6c70331f426284
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-04-08 18:38:43 -0400

    remove React.StrictMode

--- src/store/flux.js
+++ src/store/flux.js
@@ -1,6 +1,3 @@
-//const url = process.env.REACT_APP_API_URL;
-//const url_personaje = "https://api.openalbion.com/api/v3/weapons";
-
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
@@ -9,29 +6,31 @@ const getState = ({ getStore, getActions, setStore }) => {
     },
     actions: {
       // GETS personajes
-     /* getPersonajes: async () => {
-        try {
-          const response = await fetch('http://localhost:3002/api/data'); // Ruta del servidor
-          if (!response.ok) {
-            throw new Error(`Error ${response.status} al obtener los datos`);
-            console.log(response);
-          }
-          return await response.json();
-          setStore({ personajes: data });
-          console.log(store.personajes, "personajes");
-        } catch (error) {
-          console.error('Error al obtener los datos:', error);
-        }
-      }*/
-      getPersonajes: (player) => {
-        fetch(`http://localhost:3002/api/data?player=${player}`).then(data => {
-         if (!data.ok) {
-           throw new Error(`Error ${data.status} al obtener los datos`);
+      /* getPersonajes: async () => {
+         try {
+           const response = await fetch('http://localhost:3002/api/data'); // Ruta del servidor
+           if (!response.ok) {
+             throw new Error(`Error ${response.status} al obtener los datos`);
+             console.log(response);
+           }
+           return await response.json();
+           setStore({ personajes: data });
+           console.log(store.personajes, "personajes");
+         } catch (error) {
+           console.error('Error al obtener los datos:', error);
          }
-         return data.json();
-        }).then(data => {
-          setStore({ personajes: data });
-        })
+       }*/
+      getPersonajes: (player) => {
+        fetch(`http://localhost:3002/api/data?player=${player}`)
+          .then(data => {
+            if (!data.ok) {
+              throw new Error(`Error ${data.status} al obtener los datos`);
+            }
+            return data.json();
+          })
+          .then(data => {
+            setStore({ personajes: data });
+          })
       }
     },
   };
--- src/views/Personajes.js
+++ src/views/Personajes.js
@@ -1,29 +1,48 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-
 
 export const Personaje = () => {
     const { store, actions } = useContext(Context);
+    const [ personaje, setPersonaje ] = useState([]);
+    const [ searchTerm, setSearchTerm ] = useState(""); // valores de input
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            handleSearch();
+        }
+    };
 
     useEffect(() => {
-        actions.getPersonajes("pelotuda");
-    }, []);
-    console.log(store, "personajes.js");
-    if (!store.personajes.players) {
-        return <p>Cargando...</p>;
-    } else {
+        actions.getPersonajes("");
+    }, [actions]);
 
-        return (
+    return (
+        <>
             <div>
-                {store.personajes.players.map((r) => (
-                    <div key={r.Id}>
-                        <p>Nombre: {r.Name}</p>
-                        <p>ID: {r.Id}</p>
-                        <p>Guild ID: {r.GuildId}</p>
-                        <p>Guild Name: {r.GuildName}</p>
-                    </div>
-                ))}
+                <h1>Personajes</h1>
+                <input 
+                    type="text"
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Buscar personaje..."
+                />
             </div>
-        );
-    }
+            <div>
+                {store.personajes.length === 0 ? (
+                    <p>Cargando...</p>
+                ) : (
+                    store.personajes.map((player) => (
+                        <div key={player.Id}>
+                            <p>Nombre: {player.Name}</p>
+                            <p>ID: {player.Id}</p>
+                            <p>ID del gremio: {player.GuildId}</p>
+                            <p>Nombre del gremio: {player.GuildName}</p>
+                        </div>
+                    ))
+                )}
+            </div>
+        </>
+    );
 };
