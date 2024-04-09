d1ba2b06941d5fd60f18d6bfd1329aff364f011a
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-04-09 18:53:05 -0400

    add tailwind style

--- src/components/CardPlayer.js
+++ src/components/CardPlayer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const CardPlayer = ({ players }) => {
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="relative overflow-x-auto shadow-md">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
