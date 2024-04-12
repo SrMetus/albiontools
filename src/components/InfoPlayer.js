57ba08e47375cf266aa55bb006c14c704f26ee8d
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-04-12 17:56:16 -0400

    Adding style

--- src/components/InfoPlayer.js
+++ src/components/InfoPlayer.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { useParams } from "react-router-dom";
 
@@ -13,70 +13,65 @@ export const InfoPlayer = () => {
     const playerInfo = store.personajesInfo;
 
     return (
-        <div>
-            {playerInfo ? (
+        <div className="bg-gray-900 min-h-screen">
+            {playerInfo && playerInfo.LifetimeStatistics && playerInfo.LifetimeStatistics.PvE && playerInfo.LifetimeStatistics.Gathering ? (
                 <div>
-                    <h1>Informacion del juegador: {playerInfo.Name}</h1>
-                    <h3>Guild Name: {playerInfo.GuildName}</h3>
-                    <h3>Death Fame: {playerInfo.DeathFame}</h3>
-                    <h3>Kill Fame: {playerInfo.KillFame}</h3>
-                    <h3>Ratio Fame: {playerInfo.FameRatio}</h3>
-                    <h1>PvE</h1>
-                    {playerInfo.LifetimeStatistics && playerInfo.LifetimeStatistics.PvE ? (
-                        <div>
-                            <h3>Total: {playerInfo.LifetimeStatistics.PvE.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.PvE.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.PvE.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.PvE.Avalon}</h3>
-                            <h3>Hellgate: {playerInfo.LifetimeStatistics.PvE.Hellgate}</h3>
-                            <h3>CarruptedDungeon: {playerInfo.LifetimeStatistics.PvE.CorruptedDungeon}</h3>
-                            <h3>Mists: {playerInfo.LifetimeStatistics.PvE.Mists}</h3>
+                    <div className="p-4">
+                        <h1 className="text-red-500 text-2xl font-semibold">{playerInfo.Name}</h1>
+                        <h4 className="text-gray-400">Player ID: {playerInfo.Id}</h4>
+                        <h4 className="text-gray-400">Total Fame: {playerInfo.LifetimeStatistics.Gathering.All.Total}</h4>
+                    </div>
+                    <div className="flex flex-wrap">
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">PVE Fame</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.Total}</h4>
                         </div>
-                    ) : (
-                        <h3>No se han cargado datos</h3>
-                    )}
-                    <h1>Gathering</h1>
-                    {playerInfo.LifetimeStatistics && playerInfo.LifetimeStatistics.Gathering ? (
-                        <div>
-                            <h2>Fiber</h2>
-                            <h3>Total: {playerInfo.LifetimeStatistics.Gathering.Fiber.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.Gathering.Fiber.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.Gathering.Fiber.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.Gathering.Fiber.Avalon}</h3>
-                            <h2>Hide</h2>
-                            <h3>Total: {playerInfo.LifetimeStatistics.Gathering.Hide.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.Gathering.Hide.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.Gathering.Hide.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.Gathering.Hide.Avalon}</h3>
-                            <h2>Ore</h2>
-                            <h3>Total: {playerInfo.LifetimeStatistics.Gathering.Ore.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.Gathering.Ore.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.Gathering.Ore.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.Gathering.Ore.Avalon}</h3>
-                            <h2>Rock</h2>
-                            <h3>Total: {playerInfo.LifetimeStatistics.Gathering.Rock.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.Gathering.Rock.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.Gathering.Rock.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.Gathering.Rock.Avalon}</h3>
-                            <h2>Wood</h2>
-                            <h3>Total: {playerInfo.LifetimeStatistics.Gathering.Wood.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.Gathering.Wood.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.Gathering.Wood.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.Gathering.Wood.Avalon}</h3>
-                            <h2>All</h2>
-                            <h3>Total: {playerInfo.LifetimeStatistics.Gathering.All.Total}</h3>
-                            <h3>Royal: {playerInfo.LifetimeStatistics.Gathering.All.Royal}</h3>
-                            <h3>Outlands: {playerInfo.LifetimeStatistics.Gathering.All.Outlands}</h3>
-                            <h3>Avalon: {playerInfo.LifetimeStatistics.Gathering.All.Avalon}</h3>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">KILL FAME</h1>
+                            <h4 className="text-gray-400">{playerInfo.KillFame}</h4>
                         </div>
-                    ) : (
-                        <h3>No se han cargado datos</h3>
-                    )}
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">DEATH FAME</h1>
+                            <h4 className="text-gray-400">{playerInfo.DeathFame}</h4>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">RATIO FAME</h1>
+                            <h4 className="text-gray-400">{playerInfo.FameRatio}</h4>
+                        </div>
+                    </div>
+                    <hr className="border-gray-600 my-2" />
+                    <h1 className="text-orange-600 text-lg font-semibold p-4">PVE FAME SPECIFIED</h1>
+                    <div className="flex flex-wrap">
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">ROYALS</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.Royal}</h4>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">OUTLANDS</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.Outlands}</h4>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">AVALON</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.Avalon}</h4>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">HELLGATE</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.Hellgate}</h4>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">CARRUPTED DUNGEON</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.CorruptedDungeon}</h4>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+                            <h1 className="text-white text-lg font-semibold">MISTS</h1>
+                            <h4 className="text-gray-400">{playerInfo.LifetimeStatistics.PvE.Mists}</h4>
+                        </div>
+                    </div>
+                    <hr className="border-gray-600 my-2" />
                 </div>
             ) : (
                 <h1>Cargando...</h1>
             )}
         </div>
     );
-
 }
