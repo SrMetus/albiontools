4893076cddb919d8317c8d1d2fca5d31776433ea
Sebastián Gaggero Díaz <s.gaggero87@gmail.com>
2024-04-15 18:00:17 -0400

    Adding checkbox markers

--- src/views/Mercado.js
+++ src/views/Mercado.js
@@ -2,31 +2,27 @@ import React from "react";
 
 export const Mercado = () => {
     return (
-        <div className="bg-gray-900 flex">
-            <div className="flex items-center me-4">
-                <input id="caerleon-checkbox" type="checkbox" value="" className="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 rounded focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label for="caerleon-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Caerleon</label>
-            </div>
-            <div className="flex items-center me-4">
-                <input id="bridgewatch-checkbox" type="checkbox" value="" className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label for="bridgewatch-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Bridgewatch</label>
-            </div>
-            <div className="flex items-center me-4">
-                <input id="FortSterling-checkbox" type="checkbox" value="" className="w-4 h-4 text-purple-600 bg-gray-100 border-gray-300 rounded focus:ring-purple-500 dark:focus:ring-purple-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label for="FortSterling-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Fort Sterling</label>
-            </div>
-            <div className="flex items-center me-4">
-                <input id="lymhurst-checkbox" type="checkbox" value="" className="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 rounded focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label for="lymhurst-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Lymhurst</label>
-            </div>
-            <div className="flex items-center me-4">
-                <input id="martlock-checkbox" type="checkbox" value="" className="w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label for="martlock-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Martlock</label>
-            </div>
-            <div className="flex items-center me-4">
-                <input id="thetford-checkbox" type="checkbox" value="" className="w-4 h-4 text-orange-500 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 dark:focus:ring-orange-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label for="thetford-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Thetford</label>
-            </div>
+        <div className="inline-flex items-center">
+            <label className="relative flex items-center p-3 px-1 cursor-pointer" htmlFor="caerleon-checkbox">
+                <input type="checkbox" className="peer hidden" id="caerleon-checkbox" />
+                <span className="appearance-none w-15 h-5 p-1 bg-gray-900 flex items-center justify-center text-white font-medium text-sm peer-checked:bg-blue-gray-500 peer-checked:text-gray-900 rounded-md">Caerleon</span>
+            </label>
+            <label className="relative flex items-center p-3 px-1 cursor-pointer" htmlFor="bridgewatch-checkbox" >
+                <input type="checkbox" className="peer hidden" id="bridgewatch-checkbox" />
+                <span className="appearance-none w-15 h-5 p-1 bg-gray-900 flex items-center justify-center text-white font-medium text-sm peer-checked:bg-blue-gray-500 peer-checked:text-gray-900 rounded-md">Bridgewatch</span>
+            </label>
+            <label className="relative flex items-center p-3 px-1 cursor-pointer" htmlFor="fortsterling-checkbox">
+                <input type="checkbox" className="peer hidden" id="fortsterling-checkbox" />
+                <span className="appearance-none w-15 h-5 p-1 bg-blue-500 flex items-center justify-center text-white font-sm peer-checked:bg-blue-gray-500 peer-checked:text-gray-900 rounded-md">Fort Sterling</span>
+            </label>
+            <label className="relative flex items-center p-3 px-1 cursor-pointer" htmlFor="lymhurst-checkbox">
+                <input type="checkbox" className="peer hidden" id="lymhurst-checkbox" />
+                <span className="appearance-none w-15 h-5 p-1 bg-gray-900 flex items-center justify-center text-white font-medium text-sm peer-checked:bg-blue-gray-500 peer-checked:text-gray-900 rounded-md">Lymhurst</span>
+            </label>
+            <label className="relative flex items-center p-3 px-1 cursor-pointer" htmlFor="martlock-checkbox">
+                <input type="checkbox" className="peer hidden" id="martlock-checkbox" />
+                <span className="appearance-none w-15 h-5 p-1 bg-gray-900 flex items-center justify-center text-white font-medium text-sm peer-checked:bg-blue-gray-500 peer-checked:text-gray-900 rounded-md">Martlock</span>
+            </label>
         </div>
     );
 }
